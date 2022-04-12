fd5f5e2993de01a4cdb38cf77305c5da14509f7d
Ashley Lampard <ashleylampard@live.com>
2022-04-12 09:30:16 +0100

    styled answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,20 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:answer]
+    # raise
+    @answer = calc_answer(@question)
+  end
+
+  def calc_answer(question)
+    if question.downcase == 'I am going to work'.downcase
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
 end
